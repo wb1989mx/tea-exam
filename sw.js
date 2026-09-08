e98efaed2c7e6d380985e4c29c7eb6fc8e69b869
wb1989mx <904778847@qq.com>
2026-09-08 14:59:01 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * 评茶员考核系统 PWA
  * ============================================================ */
 
-var CACHE_NAME = 'tea-exam-v1';
+var CACHE_NAME = 'tea-exam-v3';
 var ASSETS = [
   './',
   './index.html',
